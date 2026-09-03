d0303e2963f6e703d19c4edbfd54406da21d5992
surya <sivasurya101010@gmail.com>
2026-09-03 15:25:18 +0530

    craeted the user table

--- users/models.py
+++ users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    profile_picture=models.ImageField(upload_to='profile_picture', null=True, blank=True)
+    bio=models.CharField(max_length=50,blank=True)
+    location=models.CharField(max_length=100,blank=True)
