527c5162a9ec7f3cb57e8ebc92d911d2906b7d56
surya <sivasurya101010@gmail.com>
2026-09-05 15:23:23 +0530

    tested jwt authentication

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
@@ -16,7 +17,14 @@ class UserSerializer(serializers.ModelSerializer):
             'bio',
             'location',]
 
-        extra_kwrgs={'password':{'write_only':True}}
+        extra_kwrgs={'password':{'write_only':True},  'validators': [validate_password],}
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("User already exists")
+        
+        return
+
 
     def create(self, validated_data):
         user=User.objects.create_user(**validated_data)
--- users/views.py
+++ users/views.py
@@ -2,6 +2,15 @@ from rest_framework import generics
 from .models import User
 from .serializers import UserSerializer
 
+from rest_framework.permissions import IsAuthenticated
+
 class RegisterView(generics.CreateAPIView):
     queryset=User.objects.all()
     serializer_class=UserSerializer
+
+class MyProfileView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
